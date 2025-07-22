7ed542581636a01ea7e94ed371c53aa4ee6f2ec4
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-07-22 14:31:14 +0300

    s

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var array = ['Button1', 'Button2', 'Button3', 'Button4', 'Button5']
 
-const arrayOfDivs = document.createElement("div")
-
 const sitesBody = document.querySelector("body")
 
 for (i = 0; i < array.length; i++) {
-    document.createElement("div", "DivArray"+i)
-    let Appendable = document.getElementById("DivArray"+i)
-    sitesBody.appendChild(Appendable)
-    console.log(array[i])
+    const div = document.createElement("div")
+    div.id = "Array"+i
+    div.textContent = "Array"+i
+    console.log('Appended: '+div)
 }
