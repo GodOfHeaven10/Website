8be48e4c0cc44b099f9d12e2212c386cba899fcf
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-07-22 14:36:29 +0300

    s

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var array = ['Button1', 'Button2', 'Button3', 'Button4', 'Button5']
+var array = ['Button1', 'Button2', 'Button3', 'Button4', 'Button5'];
 
-const sitesBody = document.querySelector("body")
+const sitesBody = document.querySelector("body");
 
 for (i = 0; i < array.length; i++) {
-    const div = document.createElement("div")
-    div.id = "Array"+i
-    div.textContent = "Array"+i
-    console.log('Appended: '+div)
+    const div = document.createElement("div");
+    document.appendChild(div);
+    div.id = "Array"+i;
+    div.textContent = "Array"+i;
+    console.log('Appended: '+div);
 }
