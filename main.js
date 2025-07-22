f874e9186eb26ab693a14165e718d0dcec0894e0
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-07-22 14:26:15 +0300

    s

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ const sitesBody = document.querySelector("body")
 for (i = 0; i < array.length; i++) {
     document.createElement("div", "DivArray"+i)
     let Appendable = document.getElementById("DivArray"+i)
-    document.appendChild(Appendable)
+    sitesBody.appendChild(Appendable)
     console.log(array[i])
 }
