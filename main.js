cef79432497e66e8961a86e38e499a309114f6dc
GodOfHeaven10 <145686810+GodOfHeaven10@users.noreply.github.com>
2025-07-22 14:55:32 +0300

    s

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ const sitesBody = document.querySelector("body");
 for (i = 0; i < array.length; i++) {
     const div = document.createElement("div");
     sitesBody.appendChild(div);
-    div.id = "Array"+i;
+    // div.id = "Array"+i;
     div.textContent = array[i];
     div.style.backgroundColor = "white";
     div.style.color = "#959595ff";
     div.style.padding = "25px"
     div.style.width = "100px"
     div.style.height = "50px"
+    div.style.textAlign = "center"
     console.log('Appended: '+div);
 }
